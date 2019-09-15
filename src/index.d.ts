83cf5f3d25feb58cc0e3d7fccd46bd9b3c4b7279
Lidor <lidor.avitan@nielsen.com>
2019-09-15 16:10:51 +0300

    feat: typescript with readme

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,17 +1,15 @@
 export interface Action<T = any> {
-    type: T;
+  type: T
+}
+interface ExtAction extends Action {
+  [extendedProps: string]: any
+}
+export declare type Reducer<S, A> = (state: S, action: A) => S
+export declare type Handlers<S, A extends Action = ExtAction> = {
+  [K in A['type']]: Reducer<S, A extends Action<K> ? A : never>
 }
 
-interface ExtendedAction extends Action {
-    [props: string]: any;
-}
-
-export declare type Reducer<S, A> = (state: S, action: A) => S;
-export declare type Handlers<S, A extends Action = ExtendedAction> = {
-    [K in A['type']]: Reducer<S, A extends Action<K> ? A : never>;
-};
-
-export declare function createReducer<S, A extends Action = ExtendedAction>(
-    initialState: S,
-    handlers: Handlers<S, A>,
-): (state: S | undefined, action: A) => any;
+export declare function createReducer<S, A extends Action = ExtAction>(
+  initialState: S,
+  handlers: Handlers<S, A>
+): (state: S | undefined, action: A) => any
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-exports = function createReducer(initialState, handlers) {
+exports.createReducer = function createReducer(initialState, handlers) {
   return function reducer(state = initialState, action) {
     if (handlers.hasOwnProperty(action.type)) {
       return handlers[action.type](state, action)
